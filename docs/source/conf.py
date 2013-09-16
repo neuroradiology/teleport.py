892be14aa20443fca9e5f7eebfe58e3af65dc3cb
Alexei Boronine <alexei.boronine@gmail.com>
2013-09-16 19:21:08 -0400

    Remove unnecessary lines from conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,12 +13,6 @@
 
 import sys, os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-source_dir = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(source_dir, '../..'))
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
